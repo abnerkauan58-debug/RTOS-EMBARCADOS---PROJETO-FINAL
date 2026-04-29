0e3ac064effa17963941a42a9d5429a635de8cec
abnerkauan58-debug <abnerkauan58@gmail.com>
2026-04-29 16:00:14 -0300

    Create storage.c

---
+++ storage.c
@@ -0,0 +1,12 @@
+#include "storage_manager.h"
+#include "nvs.h"
+
+void storage_manager_init(void)
+{
+    // Inicialização NVS
+}
+
+void storage_log_alarm(const char *alarm)
+{
+    // Salvar log
+}
